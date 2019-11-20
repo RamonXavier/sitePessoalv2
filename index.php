95312bf06521730cd7699c227161977088b96bb8
Ramon Xavier <ramon438xavier@gmail.com>
2019-11-19 23:32:08 -0300

    Inserido funções js que populam a página principal. / uml atualizado com relacionamentos finais

--- index.php
+++ index.php
@@ -30,17 +30,24 @@
 </nav>
 <div id="offcanvas-slide" uk-offcanvas>
     <div class="uk-offcanvas-bar">
-
-        <ul class="uk-nav uk-nav-default">
-            <li class="uk-active"><a href="#">Active</a></li>
-            <li><a href="#">Item</a></li>
-            <li class="uk-nav-header">Header</li>
-            <li><a href="#">Item</a></li>
-            <li><a href="#">Item</a></li>
-            <li class="uk-nav-divider"></li>
-            <li><a href="#">Item</a></li>
+    <ul class="uk-nav uk-nav-default">
+            <li class="uk-active"><a href="#">Administrativo</a></li>
+            <li><a href="#">Cadastro de Projeto</a></li>
+            <li><a href="#">Cadastro de Certificado</a></li>
+            <li><a href="#">Cadastro de Experiêcia</a></li>
+            <li><a href="#">Cadastro de Habilidade</a></li>
+            <li><a href="#">Cadastro de Dados Pessoais</a></li>
+            <li><a href="#">Cadastro de Objetivo e Escolaridade</a></li>
         </ul>
-
+        <ul class="uk-nav uk-nav-default">
+            <li class="uk-active"><a href="#">Visitante</a></li>
+            <li><a href="#">Projetos</a></li>
+            <li><a href="#">Certificados</a></li>
+            <li><a href="#">Experiêcias</a></li>
+            <li><a href="#">Habilidades</a></li>
+            <li><a href="#">Dados Pessoais</a></li>
+            <li><a href="#">Objetivos e Escolaridade</a></li>
+        </ul>        
     </div>
 </div>
 <div class="uk-container">
@@ -49,42 +56,42 @@
             <div class="uk-card uk-card-default uk-card-body">
                 <h3 class="uk-card-title"><span class="icons-cat"><i class="fas fa-award"></i> Certificados</span></h3>
                 <p id="certificado_desc"></p>
-                <p> <button class="uk-button bt-box">Danger</button></p>
+                <p> <button class="uk-button bt-box btn_visualizar"></button></p>
             </div>
         </div>
         <div>
             <div class="uk-card uk-card-primary uk-card-body">
                 <h3 class="uk-card-title"><span class="icons-cat"><i class="fas fa-layer-group"></i> Projetos</span></h3>
                 <p id="projetos_desc"></p>
-                <p> <button class="uk-button bt-box">Danger</button></p>
+                <p> <button class="uk-button bt-box btn_visualizar"></button></p>
             </div>
         </div>
         <div>
             <div class="uk-card uk-card-secondary uk-card-body">
                 <h3 class="uk-card-title"><span class="icons-cat"><i class="fab fa-battle-net"></i> Exeperiências</span></h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                <p> <button class="uk-button bt-box">Danger</button></p>
+                <p id="experiencia_desc"></p>
+                <p> <button class="uk-button bt-box btn_visualizar"></button></p>
             </div>
         </div>
         <div>
             <div class="uk-card uk-card-default uk-card-body">
                 <h3 class="uk-card-title"><span class="icons-cat"><i class="fas fa-bolt"></i> Habilidades</span></h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                <p> <button class="uk-button bt-box">Danger</button></p>
+                <p id="habilidades_desc"></p>
+                <p> <button class="uk-button bt-box btn_visualizar"></button></p>
             </div>
         </div>
         <div>
             <div class="uk-card uk-card-primary uk-card-body">
                 <h3 class="uk-card-title"><span class="icons-cat"><i class="fas fa-address-card"></i> Dados Pessoais</span></h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                <p> <button class="uk-button bt-box">Danger</button></p>
+                <p id="dados_pessoais"></p>
+                <p> <button class="uk-button bt-box btn_visualizar"></button></p>
             </div>
         </div>
         <div>
             <div class="uk-card uk-card-secondary uk-card-body">
                 <h3 class="uk-card-title"><span class="icons-cat"><i class="fas fa-brain"></i> Objetivo e Escolaridade</span></h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                <p> <button class="uk-button bt-box">Danger</button></p>
+                <p id="obj_escl"></p>
+                <p> <button class="uk-button bt-box btn_visualizar"></button></p>
             </div>
         </div>
     </div>
--- js/javascript/textos.js
+++ js/javascript/textos.js
@@ -6,6 +6,28 @@ var projeto =
   "Projetos são todas as aplicações que foram desenvolvidas. As mesmas podem possuir imagens" +
   ", descrições e explicações técnicas usadas para evolução do App.";
 
+var experiencias =
+  "Uma apresentação de todas minhas esxperiências citando facilidades e dificuldades encontradas nas" +
+  "profissões exercidas.";
+
+var habilidades =
+  "Apresentação representativa em gráficos de uma auto-avaliação de meu perfil profissional." +
+  " Abordando habilidades técnicas e pessoais.";
+
+var dados_pessoais =
+  "Me conheça melhor!" +
+  " Aqui é apresentado meus dados de contato e redes sociais, Vamos bater um papo? :)";
+
+var obj_escl =
+  "Se especializar é sempre bom. Aqui você saberá onde eu estudei e quanto tempo." +
+  " Minhas pretenções também estarão aqui, Chegue mais!";
+
+var btn_visualizar = "Visualizar";
+
 $("#certificado_desc").html(certificado);
 $("#projetos_desc").html(projeto);
-// document.getElementById("certificado_desc").innerHTML = "aa";
+$("#experiencia_desc").html(experiencias);
+$("#habilidades_desc").html(habilidades);
+$("#dados_pessoais").html(dados_pessoais);
+$("#obj_escl").html(obj_escl);
+$(".btn_visualizar").html(btn_visualizar);
